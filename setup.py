528bf122b59ca5a7ff9def7838e09e33d32feb4e
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-09-04 13:00:12 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+NAME = 'serializable'
+VERSION = '0.0'
+
+setup(
+    name=NAME,
+    packages=('serializable', ),
+    version=VERSION,
+    description="Utilities for creating serializable classes.",
+    author='Kristian Hartikainen',
+    author_email='kristian.hartikainen@gmail.com',
+)
