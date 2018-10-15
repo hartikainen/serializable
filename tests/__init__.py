d2a1bec54b0f8be893f1b4af97dc3bb9421bd02a
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-10-15 15:28:20 -0700

    Add test scaffolding for Serializable

---
+++ tests/__init__.py
@@ -0,0 +1,23 @@
+import unittest
+from serializable import Serializable
+
+
+class TestSerializable(unittest.TestCase):
+    def test_args_functionality(self):
+        pass
+
+    def test_kwargs_functionality(self):
+        pass
+
+    def test_variable_args_functionality(self):
+        pass
+
+    def test_variable_kwargs_functionality(self):
+        pass
+
+    def test_mixed_argument_types(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
