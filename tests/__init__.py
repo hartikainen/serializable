1255815567b6642fcb7401132912871996c57816
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-10-15 20:14:21 -0700

    Implement tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,11 +3,7 @@ from serializable import Serializable
 
 
 class SimpleSerializable(Serializable):
-    def __init__(self,
-                 arg1,
-                 *args,
-                 kwarg1=None,
-                 **kwargs):
+    def __init__(self, arg1, *args, kwarg1=None, **kwargs):
         self.arg1 = arg1
         self.args = args
         self.kwarg1 = kwarg1
@@ -15,24 +11,67 @@ class SimpleSerializable(Serializable):
         self._Serializable__initialize(locals())
 
 
+class ImplicitInitializationSerializable(Serializable):
+    def __init__(self, arg1, *args, kwarg1=None, **kwargs):
+        self.arg1 = arg1
+        self.args = args
+        self.kwarg1 = kwarg1
+        self.kwargs = kwargs
+        super(ImplicitInitializationSerializable, self).__init__(
+            arg1, *args, kwarg1=kwarg1, **kwargs)
+
+
+def assert_objects_match(object1, object2):
+    assert object1.arg1 == object2.arg1
+    assert object1.args == object2.args
+    assert object1.kwarg1 == object2.kwarg1
+    assert object1.kwargs == object2.kwargs
+
+
 class TestSerializable(unittest.TestCase):
     def test_args_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable('ARG1')
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_kwargs_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable('ARG1', kwarg1='KWARG1')
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_variable_args_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable('ARG1', *('ARGS[1]', 'ARGS[2]'))
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_empty_variable_args_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable(
+            'ARG1',
+            kwarg1='KWARG1',
+            **{'kwargs[1]': 'KWARGS[1]', 'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_variable_kwargs_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable(
+            'ARG1', **{'kwargs[1]': 'KWARGS[1]', 'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_empty_variable_kwargs_functionality(self):
-        pass
+        simple_object_1 = SimpleSerializable(
+            'ARG1', *('ARGS[1]', 'ARGS[2]'), kwarg1='KWARG1')
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_mixed_argument_types(self):
         simple_object_1 = SimpleSerializable(
@@ -44,22 +83,34 @@ class TestSerializable(unittest.TestCase):
 
         simple_object_2 = Serializable.clone(simple_object_1)
 
-        assert simple_object_1.arg1 == simple_object_2.arg1
-        assert simple_object_1.args == simple_object_2.args
-        assert simple_object_1.kwarg1 == simple_object_2.kwarg1
-        assert simple_object_1.kwargs == simple_object_2.kwargs
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_missing_default_values(self):
-        pass
+        simple_object_1 = SimpleSerializable(
+            'ARG1',
+            *('ARGS[1]', 'ARGS[2]'),
+            **{'kwargs[1]': 'KWARGS[1]', 'kwargs[2]': 'KWARGS[2]'}
+        )
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
 
     def test_default_initialization(self):
+        simple_object_1 = ImplicitInitializationSerializable(
+            'ARG1',
+            *('ARGS[1]', 'ARGS[2]'),
+            kwarg1='KWARG1',
+            **{'kwargs[1]': 'KWARGS[1]', 'kwargs[2]': 'KWARGS[2]'}
+        )
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_clone_non_serializable(self):
         pass
 
-    def test_raises_with_more_than_one_args(self):
-        pass
-
-    def test_raises_with_more_than_one_kwargs(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()
