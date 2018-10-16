e35380cbecf5f8b73478990afc706bc0fe017a5b
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2018-10-15 20:20:52 -0700

    Move Serializable class into its own file, rename tests accordingly

---
+++ serializable/serializable.py
@@ -0,0 +1,123 @@
+import inspect
+
+
+def parse_self_arg_key(spec, locals_, instance):
+    self_arg_key = None
+
+    if locals_[spec.args[0]] is instance:
+        self_arg_key = spec.args[0]
+
+    if self_arg_key is None:
+        if 'self' in spec.args and instance is locals_['self']:
+            self_arg_key = 'self'
+
+    if self_arg_key is None:
+        possible_self_arg_keys = [
+            arg_name for arg_name in spec.args if locals_[arg_name] is instance
+        ]
+
+        if len(possible_self_arg_keys) > 0:
+            raise ValueError(
+                "Unable to serialize and object when multiple local"
+                " variables refer to self. Typically, 'self' should refer"
+                " to the object itself, but found the following: {}"
+                "".format(possible_self_arg_keys))
+
+        self_arg_key = possible_self_arg_keys[0]
+
+    return self_arg_key
+
+
+class Serializable(object):
+
+    def __init__(self, *args, **kwargs):
+        self.__args = args
+        self.__kwargs = kwargs
+        self.__initialized = True
+
+    def __initialize(self, locals_):
+        if getattr(self, "__initialized", False):
+            return
+
+        signature = inspect.signature(self.__init__)
+        positional_keys = [
+            p.name for p in signature.parameters.values()
+            if p.kind in (p.POSITIONAL_ONLY, p.POSITIONAL_OR_KEYWORD)
+        ]
+
+        var_positional_keys = [
+            p.name for p in signature.parameters.values()
+            if p.kind == p.VAR_POSITIONAL
+        ]
+
+        keyword_keys = [
+            p.name for p in signature.parameters.values()
+            if p.kind == p.KEYWORD_ONLY
+        ]
+
+        var_keyword_keys = [
+            p.name for p in signature.parameters.values()
+            if p.kind == p.VAR_KEYWORD
+        ]
+
+        if len(var_positional_keys) > 1:
+            raise NotImplementedError(
+                "Can't yet handle more than one variable args. Got: {}"
+                "".format(var_positional_keys))
+        if len(var_keyword_keys) > 1:
+            raise NotImplementedError(
+                "Can't yet handle more than one variable kwargs. Got: {}"
+                "".format(var_keyword_keys))
+
+        positional_values = [locals_[key] for key in positional_keys]
+        var_positional_values = locals_[var_positional_keys[0]]
+        keyword_values = {key: locals_[key] for key in keyword_keys}
+        var_keyword_values = locals_[var_keyword_keys[0]]
+
+        bound_arguments = signature.bind(*positional_values,
+                                         *var_positional_values,
+                                         **keyword_values, **var_keyword_values)
+
+        self.__args = bound_arguments.args
+        self.__kwargs = bound_arguments.kwargs
+
+        self.__initialized = True
+
+    def __getstate__(self):
+        assert self.__initialized, (
+            "Cannot get state from uninitialized Serializable. Forgot to call"
+            " `self._Serializable__initialize` in your __init__ method?")
+
+        state = {
+            '__args': self.__args,
+            '__kwargs': self.__kwargs,
+        }
+
+        return state
+
+    def __setstate__(self, state):
+        assert self.__initialized, (
+            "Cannot set state of uninitialized Serializable. Forgot to call"
+            " `self._Serializable__initialize` in your __init__ method?")
+
+        out = type(self)(*state["__args"], **state["__kwargs"])
+        self.__dict__.update(out.__dict__)
+
+    @staticmethod
+    def clone(instance, **kwargs):
+        assert isinstance(instance, Serializable), (
+            "Can only clone Serializable objects. Got: {}"
+            "".format(type(instance)))
+        assert getattr(instance, '_Serializable__initialized', False), (
+            "Cannot clone an uninitialized Serializable. Forgot to call"
+            " `self._Serializable__initialize` in your __init__ method?")
+
+        state = instance.__getstate__()
+
+        signature = inspect.signature(instance.__init__)
+        arguments = signature.bind(*state['__args'], **state['__kwargs'])
+        arguments.apply_defaults()
+
+        out = type(instance)(*arguments.args, **arguments.kwargs)
+
+        return out
---
+++ tests/serializable.py
@@ -0,0 +1,120 @@
+import unittest
+from serializable import Serializable
+
+
+class SimpleSerializable(Serializable):
+
+    def __init__(self, arg1, *args, kwarg1=None, **kwargs):
+        self.arg1 = arg1
+        self.args = args
+        self.kwarg1 = kwarg1
+        self.kwargs = kwargs
+        self._Serializable__initialize(locals())
+
+
+class ImplicitInitializationSerializable(Serializable):
+
+    def __init__(self, arg1, *args, kwarg1=None, **kwargs):
+        self.arg1 = arg1
+        self.args = args
+        self.kwarg1 = kwarg1
+        self.kwargs = kwargs
+        super(ImplicitInitializationSerializable, self).__init__(
+            arg1, *args, kwarg1=kwarg1, **kwargs)
+
+
+def assert_objects_match(object1, object2):
+    assert object1.arg1 == object2.arg1
+    assert object1.args == object2.args
+    assert object1.kwarg1 == object2.kwarg1
+    assert object1.kwargs == object2.kwargs
+
+
+class TestSerializable(unittest.TestCase):
+
+    def test_args_functionality(self):
+        simple_object_1 = SimpleSerializable('ARG1')
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_kwargs_functionality(self):
+        simple_object_1 = SimpleSerializable('ARG1', kwarg1='KWARG1')
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_variable_args_functionality(self):
+        simple_object_1 = SimpleSerializable('ARG1', *('ARGS[1]', 'ARGS[2]'))
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_empty_variable_args_functionality(self):
+        simple_object_1 = SimpleSerializable(
+            'ARG1',
+            kwarg1='KWARG1',
+            **{'kwargs[1]': 'KWARGS[1]',
+               'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_variable_kwargs_functionality(self):
+        simple_object_1 = SimpleSerializable(
+            'ARG1', **{'kwargs[1]': 'KWARGS[1]',
+                       'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_empty_variable_kwargs_functionality(self):
+        simple_object_1 = SimpleSerializable(
+            'ARG1', *('ARGS[1]', 'ARGS[2]'), kwarg1='KWARG1')
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_mixed_argument_types(self):
+        simple_object_1 = SimpleSerializable(
+            'ARG1',
+            *('ARGS[1]', 'ARGS[2]'),
+            kwarg1='KWARG1',
+            **{'kwargs[1]': 'KWARGS[1]',
+               'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_missing_default_values(self):
+        simple_object_1 = SimpleSerializable(
+            'ARG1', *('ARGS[1]', 'ARGS[2]'),
+            **{'kwargs[1]': 'KWARGS[1]',
+               'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_default_initialization(self):
+        simple_object_1 = ImplicitInitializationSerializable(
+            'ARG1',
+            *('ARGS[1]', 'ARGS[2]'),
+            kwarg1='KWARG1',
+            **{'kwargs[1]': 'KWARGS[1]',
+               'kwargs[2]': 'KWARGS[2]'})
+
+        simple_object_2 = Serializable.clone(simple_object_1)
+
+        assert_objects_match(simple_object_1, simple_object_2)
+
+    def test_clone_non_serializable(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
